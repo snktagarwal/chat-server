617b1b6305de4d47eec08aed185b8fc6325b9bfa
sanket <sanket@sanketagarwal.com>
2010-11-18 00:39:48 +0530

    license!
    Add license files to the code. Now you cannot copy the assignment and
    tell that I wasn't the author. Cool eh ;)

--- serv-client.h
+++ serv-client.h
@@ -1,4 +1,23 @@
+
 /* Defines the common header types for both the server and the client */
+
+/*  
+    Copyright (C) <2010>  <Sanket Agarwal>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 
 #define WM_MAX	500	// Welcome message size
 #define M_MAX   500	// General message size
